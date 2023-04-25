18225ed40d6f86eb44fdd82eb899689f89e4061d
Varvara Petrova <varvara25petrova@gmail.com>
2023-04-25 19:06:15 +0300

    changed now

--- script/script.js
+++ script/script.js
@@ -1,73 +1,3 @@
-// let content = document.querySelector('.content');
-// let elementGroupHeart = content.querySelector('element__group-heart');
-// let actionButton = content.querySelector('.element__group-heart_active');
-// let addForm = document.querySelector ('.profile__info-edit-button');
-
-// let popup = document.querySelector ('.popup');
-// let popupOpened = document.querySelector ('.popup_opened');
-// let popupContainer = document.querySelector ('.popup__container');
-// let profileInfoEditButton = document.querySelector ('.profile__info-edit-button');
-// let popupClose = document.querySelector ('.popup__close');
-// let profileInfoText_first = document.getElementById('profile__info-text_first');
-
-// if (profileInfoEditButton === 0) {
-//   profileInfoEditButton.setAttribute ('disabled','disabled');
-//   profileInfoEditButton.classList.add (`profile__info-edit-button_disabled`);
-// } else {
-//   profileInfoEditButton.removeAttribute ('disabled');
-//   profileInfoEditButton.classList.remove ('profile__info-edit-button_disabled');
-// }
-
-// profileInfoEditButton.forEach((button) => {
-//   button.addEventListener ('click', (e) => {
-//     e.preventDefault();
-//     popup.classList.add('active');
-//     popupContainer.classList.add('active');
-//   })
-  
-// });
-
-// profileInfoEditButton.foreach((button) => {
-//     button.addEventListener ('click', (e) => { 
-//       e.preventDefault ();
-//       popup.classList.add ('active');
-//       popupContainer.classList.add ('active');
-//     }) 
-// });
-
-// popupClose.addEventListener ('click',() => {
-//   popup.classList.remove ('active');
-//   popupContainer.classList.remove ('active');
-// });
-
-// document.addEventListener ('click',(e) => {
-//   if (e.target === popup) {
-//     popup.classList.remove ('active');
-//     popup.classList.remove ('active');
-//   }
-// });
-
-// function addText() {
-//   profileInfoText_first.innerHTML += ``;
-// }
-// addText ();
-
-// console.log(profileInfoText_first.textContent);
-// profileInfoText_first.textContent = '';
-
-// popupOpened.addEventListener('click', profileInfoEditButton);
-
-
-// profileInfoEditButton.addEventListener('click', function() {
-//   popup = `active` 
-
-// })
-
-
-// profileInfoEditButton.addEventListener ('click', profileInfoEditButton);
-
-console.log ("Привет, мир!");
-
 const content = document.querySelector('.content');
 const editButton = content.querySelector('.profile-info__edit-button');
 const popupOpenButtonElement = document.querySelector('.popup_open');
@@ -81,13 +11,6 @@ const textName = document.querySelector('.profile-info__text-name');
 const textAbout = document.querySelector('.profile-info__text-about');
 const buttonSubmit = document.querySelector('.submit-button');
 
-
-// const togglePopupVisibility = function (event) {
-//   console.log('togglePopupVisibility');
-//   popupElement.classList.toggle('popup_open');
-// }
-// togglePopupVisibility()
-
 const openPopup = function () {
   console.log('openPopup');
   popupElement.classList.add('popup_open')
@@ -99,8 +22,6 @@ const closePopup = function () {
   console.log('closePopup');
   popupElement.classList.remove('popup_open')
 }
-
-
 
 const closePopupByClickOnOverlay = function (event) {
   console.log('target', event.target);
